f531f50127ab4364744b019f48e37f1ba8eb5170
Daria <daryalari1@gmail.com>
2018-10-29 14:33:12 +0300

    Move providers from App to Index

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import { Redirect, Route, Router, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import AuthorizationPage from './components/AuthorizationPage'
@@ -9,29 +8,22 @@ import LandingPage from './components/LandingPage'
 import AuthorizedRoute from './components/AuthorizedRoute'
 import styles from "./style.css"
 
-import { store } from './store'
-import { history } from './helpers/history'
-
 class App extends React.Component {
   render() {
       return (
-          <Provider store={store}>
-            <Router history={history}>
-              <React.Fragment>
-                <Header/>
-                  <main>
-                    <Switch>
-                      <Route exact path="/" component={LandingPage}/>
-                      <AuthorizedRoute path="/dashboard" component={DashboardPage}/>
-                      <Route path="/login" component={AuthorizationPage}/>
-                      <Route path="/register" component={AuthorizationPage}/>
-                      <Redirect to="/dashboard"/>
-                    </Switch>
-                  </main>
-                <Footer/>
-              </React.Fragment>
-            </Router>
-          </Provider>
+        <React.Fragment>
+          <Header/>
+            <main>
+              <Switch>
+                <Route exact path="/" component={LandingPage}/>
+                <AuthorizedRoute path="/dashboard" component={DashboardPage}/>
+                <Route path="/login" component={AuthorizationPage}/>
+                <Route path="/register" component={AuthorizationPage}/>
+                <Redirect to="/dashboard"/>
+              </Switch>
+            </main>
+          <Footer/>
+        </React.Fragment>
       )
   }
 }

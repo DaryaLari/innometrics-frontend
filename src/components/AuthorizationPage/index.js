6893f30671415f10eb0b91c122dc8446a34f7be7
Daria <daryalari1@gmail.com>
2018-10-29 14:40:37 +0300

    Custom Input added

--- src/components/AuthorizationPage/index.js
+++ src/components/AuthorizationPage/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import {Field, isInvalid, reduxForm} from "redux-form";
+import Input from "../Input";
 import {loginRequest, registerRequest} from "../../store/user/actionCreators";
 import {email, required} from "../../helpers/formValidators";
 import styles from "./style.css";
@@ -16,24 +17,22 @@ class LoginPage extends React.Component {
               onSubmit={this.props.handleSubmit(this.onSubmit)}
         >
           <h1>Login</h1>
-          <div className={styles.inputRow}>
-            <Field
-                name="email"
-                component="input"
-                placeholder="email"
-                type="email"
-                validate={[required, email]}
-            />
-          </div>
-          <div className={styles.inputRow}>
-            <Field
-                name="password"
-                component="input"
-                placeholder="password"
-                type="password"
-                validate={required}
-            />
-          </div>
+          <Field
+            name="email"
+            component={Input}
+            label="Email"
+            placeholder="Email"
+            type="email"
+            validate={[required, email]}
+          />
+          <Field
+            name="password"
+            component={Input}
+            label="Password"
+            placeholder="Password"
+            type="password"
+            validate={required}
+          />
           <button className={styles.submitBtn}
                   type="submit"
                   disabled={this.props.submitDisabled}
@@ -58,43 +57,38 @@ class RegisterPage extends React.Component {
               onSubmit={this.props.handleSubmit(this.onSubmit)}
         >
           <h1>Register</h1>
-          <div className={styles.inputRow}>
-            <Field
-                name="email"
-                component="input"
-                placeholder="email"
-                type="email"
-                validate={[required, email]}
-            />
-          </div>
-          <div className={styles.inputRow}>
-            <Field
-                name="name"
-                component="input"
-                placeholder="name"
-                type="text"
-                validate={required}
-            />
-          </div>
-          <div className={styles.inputRow}>
-            <Field
-                name="surname"
-                component="input"
-                placeholder="surname"
-                type="text"
-                validate={required}
-            />
-          </div>
-          <div className={styles.inputRow}>
-            <Field
-                name="password"
-                component="input"
-                placeholder="password"
-                type="password"
-                validate={required}
-            />
-          </div>
-          <p className={styles.errorMsg}>{}</p>
+          <Field
+            name="email"
+            component={Input}
+            label="Email"
+            placeholder="Email"
+            type="email"
+            validate={[required, email]}
+          />
+          <Field
+            name="name"
+            component={Input}
+            label="Name"
+            placeholder="Name"
+            type="text"
+            validate={required}
+          />
+          <Field
+            name="surname"
+            component={Input}
+            label="Surname"
+            placeholder="Surname"
+            type="text"
+            validate={required}
+          />
+          <Field
+            name="password"
+            component={Input}
+            label="Password"
+            placeholder="Password"
+            type="password"
+            validate={required}
+          />
           <button className={styles.submitBtn}
                   type="submit"
                   disabled={this.props.submitDisabled}
